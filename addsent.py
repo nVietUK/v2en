00a76769c532f1e00e55f49b64d8afc7a7609551
nVietUK <vietuk.nguyen.git@icloud.com>
2023-06-25 15:24:42 +0700

    update addsent.py, en.dic, vi.dic and v2enlib.py

--- addsent.py
+++ addsent.py
@@ -1,303 +1,34 @@
-import deep_translator.exceptions, langcodes, gc
-import yaml, string, translators.server, signal, execjs._exceptions, deep_translator, time
-from translators.server import TranslatorsServer
-from tabulate import tabulate
-from multiprocessing.pool import Pool, ThreadPool
-from tqdm import tqdm
+import yaml, signal, time, gc
+from multiprocessing.pool import Pool
 
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 target = cfg["v2en"]["target"]
 num_sent = cfg["v2en"]["num_sent"]
-table_name = cfg["sqlite"]["table_name"]
-accept_percentage = cfg["v2en"]["accept_percentage"]
-thread_alow = cfg["v2en"]["thread"]["allow"]
-thread_limit = cfg["v2en"]["thread"]["limit"]
-trans_timeout = cfg["v2en"]["trans_timeout"]
 first_lang = target[:2]
 second_lang = target[-2:]
+table_name = cfg["sqlite"]["table_name"]
 first_dictionary_path = f"./cache/{first_lang}.dic"
 second_dictionary_path = f"./cache/{second_lang}.dic"
 main_execute = True
 false_allow = num_sent / 2 * 3
-translator = TranslatorsServer()
+
 
 from v2enlib import (
     printError,
-    func_timeout,
-    convert,
-    isExistOnWiki,
-    diffratio,
     loadDictionary,
     checkLangFile,
     getSQLCursor,
     createSQLtable,
     isEmpty,
     saveDictionary,
-    measure,
+    addSentExecutor,
     createOBJPool,
-    terminalWidth,
     playSound,
+    funcPool,
     InputSent,
     logger,
 )
-
-
-# thread utils
-def funcPool(func, cmds, executor, isAllowThread=True, strictOrder=False)->list:
-    try:
-        with executor(
-            min(len(cmds), thread_limit if thread_limit > 0 else len(cmds)),
-        ) as ex:
-            if not thread_alow or not isAllowThread:
-                return [func(cmd) for cmd in tqdm(cmds, leave=False)]
-            with tqdm(total=len(cmds), leave=False) as pbar:
-                results = []
-                for res in (
-                    ex.imap(func, cmds)
-                    if strictOrder
-                    else ex.imap_unordered(func, cmds)
-                ):
-                    pbar.update(1)
-                    results.append(res)
-                return results
-    except Exception as e:
-        printError("funcPool", e, False)
-    return []
-
-
-# translate def
-def deepTransGoogle(x: str, source: str, target: str) -> str:
-    try:
-        return deep_translator.GoogleTranslator(source=source, target=target).translate(
-            x
-        )
-    except deep_translator.exceptions.LanguageNotSupportedException:
-        return deepTransGoogle(
-            x, str(langcodes.Language.get(source)), str(langcodes.Language.get(target))
-        )
-    except Exception as e:
-        printError(deepTransGoogle.__name__, e, False)
-        return ""
-
-
-@measure
-def translatorsTrans(
-    trans, cmd: list, trans_timeout, tname: str = "", isDived: bool = False
-) -> list:
-    try:
-        ou = func_timeout(
-            trans_timeout,
-            trans,
-            query_text=cmd[0],
-            from_language=cmd[1],
-            to_language=cmd[2],
-            default_value="",
-        )
-        return [ou, tname]
-    except translators.server.TranslatorError as e:
-        if not isDived:
-            return _extracted_from_translatorsTrans_13(
-                cmd, e, trans, trans_timeout / 2, tname
-            )
-    except (execjs._exceptions.RuntimeUnavailableError, TypeError, ValueError):
-        pass
-    except Exception as e:
-        printError(translatorsTrans.__name__, e, False)
-    return [deepTransGoogle(*cmd), "google"]
-
-
-# TODO Rename this here and in `translatorsTrans`
-def _extracted_from_translatorsTrans_13(cmd, e, trans, trans_timeout, tname) -> list:
-    try:
-        tcmd, execute = cmd.copy(), False
-        if (e.args[0]).find("vie") != -1:
-            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vie"
-            execute = True
-
-        if (e.args[0]).find("vi_VN") != -1:
-            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vi_VN"
-            execute = True
-
-        if (e.args[0]).find("vi-VN") != -1:
-            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vi-VN"
-            execute = True
-        return (
-            translatorsTrans(trans, tcmd, trans_timeout, tname, True)
-            if execute
-            else ["", ""]
-        )
-    except IndexError:
-        return ["", ""]
-
-
-def translatorsTransExecutor(cmd, *args, **kwargs):
-    return translatorsTrans(*cmd, *args, **kwargs)
-
-
-@measure
-def transIntoList(sent, source_lang, target_lang, target_dictionary):
-    return funcPool(
-        checkSpellingExecutor,
-        [
-            [convert(e[0]), target_dictionary, target_lang, e[1]]
-            for e in funcPool(
-                translatorsTransExecutor,
-                [
-                    [
-                        trans,
-                        [sent, source_lang, target_lang],
-                        trans_timeout,
-                        name,
-                    ]
-                    for name, trans in translator.translators_dict.items()
-                ],
-                ThreadPool,
-            )
-        ],
-        ThreadPool,
-    )
-
-
-def transIntoListExecutor(cmd):
-    return transIntoList(*cmd)
-
-
-# language utils
-def checkSpelling(
-    text: str, dictionary: list, lang: str, tname: str = ""
-) -> str | list:
-    word = ""
-    try:
-        words = text.split()
-        outstr = ""
-        for idx, word in enumerate(words):
-            if (
-                word in dictionary
-                or word.isnumeric()
-                or word in string.punctuation
-                or isExistOnWiki(word, lang)
-                or isExistOnWiki(f"{words[idx-1]} {word}", lang)
-                or (
-                    idx + 1 < len(words)
-                    and isExistOnWiki(f"{word} {words[idx+1]}", lang)
-                )
-            ):
-                outstr += f"{word} "
-            else:
-                raise ValueError(f"{word} not existed")
-            if word.isalpha() and word not in dictionary:
-                dictionary.insert(0, word)
-        return [outstr, tname] if tname else outstr
-    except ValueError:
-        printError(
-            f"add word for {lang}",
-            Exception(f"{word} isn't existed on Wikitionary!"),
-            False,
-        )
-    except Exception as e:
-        printError(checkSpelling.__name__, e)
-    return ["", ""] if tname else ""
-
-
-def checkSpellingExecutor(cmd):
-    return checkSpelling(*cmd)
-
-
-@measure
-def addSent(input_sent: InputSent):
-    is_agree, first_dump_sent, second_dump_sent, cmds, trans_data = (
-        False,
-        "",
-        "",
-        [],
-        [],
-    )
-    input_sent.first, input_sent.second = funcPool(
-        checkSpellingExecutor,
-        [
-            [convert(input_sent.first.replace("\n", "")), first_dictionary, first_lang],
-            [
-                convert(input_sent.second.replace("\n", "")),
-                second_dictionary,
-                second_lang,
-            ],
-        ],
-        ThreadPool,
-        strictOrder=True
-    )
-    if input_sent.isValid():
-        is_error = True
-        first_trans, second_trans = funcPool(
-            transIntoListExecutor,
-            [
-                [input_sent.first, first_lang, second_lang, second_dictionary],
-                [input_sent.second, second_lang, first_lang, first_dictionary],
-            ],
-            ThreadPool,
-        )
-        trans_data = [
-            InputSent(
-                input_sent.first,
-                first_tran[0],
-                first_tran[1],
-                diffratio(input_sent.second, first_tran[0]),
-            )
-            for first_tran in first_trans
-            if first_tran[0]
-        ] + [
-            InputSent(
-                second_tran[0],
-                input_sent.second,
-                second_tran[1],
-                diffratio(input_sent.first, second_tran[0]),
-            )
-            for second_tran in second_trans
-            if second_tran[0]   
-        ]
-
-        if any(e.isAdd for e in trans_data):
-            is_agree = True
-            is_error = False
-        if is_agree and not is_error:
-            table_command = """
-                INSERT INTO {}(Source, Target, Verify)
-                VALUES(?,?,?)
-            """
-            cmds = [
-                [
-                    table_command.format(table_name),
-                    (input_sent.first, input_sent.second, 1),
-                ]
-            ] + [
-                [
-                    table_command.format(table_name),
-                    e.SQLFormat(),
-                ]
-                for e in trans_data
-                if e.isAdd
-            ]
-        if is_error:
-            first_dump_sent, second_dump_sent = input_sent.first, input_sent.second
-
-        print_data = [["Data set", input_sent.first, input_sent.second, "N/A"]] + [
-            [e.isFrom, e.first, e.second, e.accurate] for e in trans_data
-        ]
-        width = int(terminalWidth() / 4)
-        logger.log(
-            101,
-            tabulate(
-                tabular_data=print_data,
-                headers=["From", "Source", "Target", "Accuracy?"],
-                tablefmt="fancy_grid",
-                showindex="always",
-                maxcolwidths=[None, None, width, width, 7],
-                floatfmt=(".2f" * 5),
-            ),
-        )
-    del trans_data
-    gc.collect()
-    return first_dump_sent, second_dump_sent, cmds, is_agree
 
 
 def signalHandler(sig, frame):
@@ -305,10 +36,6 @@ def signalHandler(sig, frame):
     if main_execute:
         logger.log(101, "\tStop program!")
         main_execute = False
-
-
-def addSentExecutor(cmd):
-    return addSent(cmd)
 
 
 if __name__ == "__main__":
@@ -338,11 +65,11 @@ if __name__ == "__main__":
         for e in funcPool(
             addSentExecutor,
             [
-                InputSent(saveIN[idx], saveOU[idx])
+                [InputSent(saveIN[idx], saveOU[idx]), first_dictionary, second_dictionary]
                 for idx in range(num_sent if len(saveIN) > num_sent else len(saveIN))
             ],
             Pool,
-            strictOrder=True
+            strictOrder=True,
         ):
             if e[0] != "" and e[1] != "":
                 first_dump_sent.append(e[0])
--- v2enlib.py
+++ v2enlib.py
@@ -1,6 +1,10 @@
-import os, string, httpx, langcodes, sqlite3, resource, time, yaml, logging, librosa
+import os, string, httpx, sqlite3, resource, time, yaml, logging, librosa, string, gc
+import deep_translator.exceptions, langcodes, translators.server, execjs._exceptions, deep_translator
 from difflib import SequenceMatcher
 from multiprocess.pool import ThreadPool, TimeoutError as TLE
+from tabulate import tabulate
+from tqdm import tqdm
+from translators.server import TranslatorsServer
 
 
 # classes
@@ -23,6 +27,19 @@ class InputSent:
 
     def SQLFormat(self) -> tuple:
         return (self.first, self.second, 1 if self.isAdd else 0)
+
+
+# executors
+def transIntoListExecutor(cmd):
+    return transIntoList(*cmd)
+
+
+def addSentExecutor(cmd):
+    return addSent(*cmd)
+
+
+def translatorsTransExecutor(cmd, *args, **kwargs):
+    return translatorsTrans(*cmd, *args, **kwargs)
 
 
 # utils
@@ -153,6 +170,29 @@ def terminalWidth():
     return os.get_terminal_size().columns
 
 
+# thread utils
+def funcPool(func, cmds, executor, isAllowThread=True, strictOrder=False) -> list:
+    try:
+        with executor(
+            min(len(cmds), thread_limit if thread_limit > 0 else len(cmds)),
+        ) as ex:
+            if not thread_alow or not isAllowThread:
+                return [func(cmd) for cmd in tqdm(cmds, leave=False)]
+            with tqdm(total=len(cmds), leave=False) as pbar:
+                results = []
+                for res in (
+                    ex.imap(func, cmds)
+                    if strictOrder
+                    else ex.imap_unordered(func, cmds)
+                ):
+                    pbar.update(1)
+                    results.append(res)
+                return results
+    except Exception as e:
+        printError("funcPool", e, False)
+    return []
+
+
 # debug def
 def printError(text, error, is_exit=True):
     if error == OSError:
@@ -166,6 +206,232 @@ def printError(text, error, is_exit=True):
 
 def printInfo(name, pid):
     logger.info(f"Dive into {name} with pid id: {pid}")
+
+
+# translate def
+def deepTransGoogle(x: str, source: str, target: str) -> str:
+    try:
+        return deep_translator.GoogleTranslator(source=source, target=target).translate(
+            x
+        )
+    except deep_translator.exceptions.LanguageNotSupportedException:
+        return deepTransGoogle(
+            x, str(langcodes.Language.get(source)), str(langcodes.Language.get(target))
+        )
+    except Exception as e:
+        printError(deepTransGoogle.__name__, e, False)
+        return ""
+
+
+@measure
+def translatorsTrans(
+    trans, cmd: list, trans_timeout, tname: str = "", isDived: bool = False
+) -> list:
+    try:
+        ou = func_timeout(
+            trans_timeout,
+            trans,
+            query_text=cmd[0],
+            from_language=cmd[1],
+            to_language=cmd[2],
+            default_value="",
+        )
+        return [ou, tname]
+    except translators.server.TranslatorError as e:
+        if not isDived:
+            return _extracted_from_translatorsTrans_13(
+                cmd, e, trans, trans_timeout / 2, tname
+            )
+    except (execjs._exceptions.RuntimeUnavailableError, TypeError, ValueError):
+        pass
+    except Exception as e:
+        printError(translatorsTrans.__name__, e, False)
+    return [deepTransGoogle(*cmd), "google"]
+
+
+# TODO Rename this here and in `translatorsTrans`
+def _extracted_from_translatorsTrans_13(cmd, e, trans, trans_timeout, tname) -> list:
+    try:
+        tcmd, execute = cmd.copy(), False
+        if (e.args[0]).find("vie") != -1:
+            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vie"
+            execute = True
+
+        if (e.args[0]).find("vi_VN") != -1:
+            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vi_VN"
+            execute = True
+
+        if (e.args[0]).find("vi-VN") != -1:
+            tcmd[2 if (e.args[0]).find("to_language") != -1 else 1] = "vi-VN"
+            execute = True
+        return (
+            translatorsTrans(trans, tcmd, trans_timeout, tname, True)
+            if execute
+            else ["", ""]
+        )
+    except IndexError:
+        return ["", ""]
+
+
+@measure
+def transIntoList(sent, source_lang, target_lang, target_dictionary):
+    return funcPool(
+        checkSpellingExecutor,
+        [
+            [convert(e[0]), target_dictionary, target_lang, e[1]]
+            for e in funcPool(
+                translatorsTransExecutor,
+                [
+                    [
+                        trans,
+                        [sent, source_lang, target_lang],
+                        trans_timeout,
+                        name,
+                    ]
+                    for name, trans in translator.translators_dict.items()
+                ],
+                ThreadPool,
+            )
+        ],
+        ThreadPool,
+    )
+
+
+# language utils
+def checkSpelling(
+    text: str, dictionary: list, lang: str, tname: str = ""
+) -> str | list:
+    word = ""
+    try:
+        words = text.split()
+        outstr = ""
+        for idx, word in enumerate(words):
+            if (
+                word in dictionary
+                or word.isnumeric()
+                or word in string.punctuation
+                or isExistOnWiki(word, lang)
+                or isExistOnWiki(f"{words[idx-1]} {word}", lang)
+                or (
+                    idx + 1 < len(words)
+                    and isExistOnWiki(f"{word} {words[idx+1]}", lang)
+                )
+            ):
+                outstr += f"{word} "
+            else:
+                raise ValueError(f"{word} not existed")
+            if word.isalpha() and word not in dictionary:
+                dictionary.insert(0, word)
+        return [outstr, tname] if tname else outstr
+    except ValueError:
+        printError(
+            f"add word for {lang}",
+            Exception(f"{word} isn't existed on Wikitionary!"),
+            False,
+        )
+    except Exception as e:
+        printError(checkSpelling.__name__, e)
+    return ["", ""] if tname else ""
+
+
+def checkSpellingExecutor(cmd):
+    return checkSpelling(*cmd)
+
+
+@measure
+def addSent(input_sent: InputSent, first_dictionary, second_dictionary):
+    is_agree, first_dump_sent, second_dump_sent, cmds, trans_data = (
+        False,
+        "",
+        "",
+        [],
+        [],
+    )
+    input_sent.first, input_sent.second = funcPool(
+        checkSpellingExecutor,
+        [
+            [convert(input_sent.first.replace("\n", "")), first_dictionary, first_lang],
+            [
+                convert(input_sent.second.replace("\n", "")),
+                second_dictionary,
+                second_lang,
+            ],
+        ],
+        ThreadPool,
+        strictOrder=True,
+    )
+    if input_sent.isValid():
+        is_error = True
+        first_trans, second_trans = funcPool(
+            transIntoListExecutor,
+            [
+                [input_sent.first, first_lang, second_lang, second_dictionary],
+                [input_sent.second, second_lang, first_lang, first_dictionary],
+            ],
+            ThreadPool,
+        )
+        trans_data = [
+            InputSent(
+                input_sent.first,
+                first_tran[0],
+                first_tran[1],
+                diffratio(input_sent.second, first_tran[0]),
+            )
+            for first_tran in first_trans
+            if first_tran[0]
+        ] + [
+            InputSent(
+                second_tran[0],
+                input_sent.second,
+                second_tran[1],
+                diffratio(input_sent.first, second_tran[0]),
+            )
+            for second_tran in second_trans
+            if second_tran[0]
+        ]
+
+        if any(e.isAdd for e in trans_data):
+            is_agree = True
+            is_error = False
+        if is_agree and not is_error:
+            table_command = """
+                INSERT INTO {}(Source, Target, Verify)
+                VALUES(?,?,?)
+            """
+            cmds = [
+                [
+                    table_command.format(table_name),
+                    (input_sent.first, input_sent.second, 1),
+                ]
+            ] + [
+                [
+                    table_command.format(table_name),
+                    e.SQLFormat(),
+                ]
+                for e in trans_data
+                if e.isAdd
+            ]
+        if is_error:
+            first_dump_sent, second_dump_sent = input_sent.first, input_sent.second
+
+        print_data = [["Data set", input_sent.first, input_sent.second, "N/A"]] + [
+            [e.isFrom, e.first, e.second, e.accurate] for e in trans_data
+        ]
+        width = int(terminalWidth() / 4)
+        logger.log(
+            101,
+            tabulate(
+                tabular_data=print_data,
+                headers=["From", "Source", "Target", "Accuracy?"],
+                tablefmt="fancy_grid",
+                showindex="always",
+                maxcolwidths=[None, None, width, width, 7],
+                floatfmt=(".2f" * 5),
+            ),
+        )
+    del trans_data
+    gc.collect()
+    return first_dump_sent, second_dump_sent, cmds, is_agree
 
 
 # sqlite3 defs
@@ -216,9 +482,16 @@ def getSQL(conn, request):
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 target = cfg["v2en"]["target"]
+first_lang = target[:2]
+second_lang = target[-2:]
 accept_percentage = cfg["v2en"]["accept_percentage"]
 time_allow = cfg["v2en"]["time_allow"]
 resource_allow = cfg["v2en"]["resource_allow"]
+thread_alow = cfg["v2en"]["thread"]["allow"]
+thread_limit = cfg["v2en"]["thread"]["limit"]
+table_name = cfg["sqlite"]["table_name"]
+translator = TranslatorsServer()
+trans_timeout = cfg["v2en"]["trans_timeout"]
 
 # logger init
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s\n%(message)s")
