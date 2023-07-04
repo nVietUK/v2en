853deb7f4ec6733e3d2118008980a3d96b795f36
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-07-04 10:24:37 +0700

    create 2 files, delete 2 files, move 1 file and update 2 files

---
+++ webserver/quasar/src/components/ReadMeViewer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :style="{ height: contentHeight + 'px' }" v-html="content"></div>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+
+async function fetchContent(url: string): Promise<string> {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+import { defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+  name: 'ReadMeViewer',
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const content = ref('');
+    const contentHeight = ref(0);
+
+    onMounted(async () => {
+      content.value = await fetchContent(props.url);
+      contentHeight.value = document.body.scrollHeight;
+    });
+
+    return {
+      content,
+      contentHeight,
+    };
+  },
+});
+</script>
--- webserver/quasar/src/pages/IndexPage.vue
+++ webserver/quasar/src/pages/IndexPage.vue
@@ -2,12 +2,7 @@
   <div class="row">
     <div class="col"></div>
     <div class="col-6 position-relative">
-      <iframe
-        id="my-iframe"
-        src="https://htmlpreview.github.io/?https://raw.githubusercontent.com/TakahashiNguyen/TakahashiNguyen/main/README.md"
-        style="width: 100%; border: none"
-        onload="resizeIframe"
-      ></iframe>
+      <ReadMeViewer :url="link" />
     </div>
     <div class="col"></div>
   </div>
@@ -15,30 +10,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
-async function fetchContent() {
-  const url = 'https://example.com/content-to-copy';
-
-  const response = await fetch(url);
-  const html = await response.text();
-
-  const container = document.createElement('div');
-  container.innerHTML = html;
-
-  const content = container.querySelector('#content-to-copy');
-
-  if (content) {
-    const height = content. | 0;
-    content.style.height = height + 'px';
-
-    return content.outerHTML;
-  }
-}
-
+import ReadMeViewer from 'components/ReadMeViewer.vue';
 export default defineComponent({
   name: 'IndexPage',
+  components: {
+    ReadMeViewer,
+  },
   setup() {
-    return {fetchContent()}
-  }
-})
+    const link =
+      'https://raw.githubusercontent.com/TakahashiNguyen/TakahashiNguyen/main/README.md';
+    return { link };
+  },
+});
 </script>
