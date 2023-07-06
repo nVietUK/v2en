1c94fa1c52c418631372cc24af1441e010881e98
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-07-06 09:25:09 +0700

    build(deps): update package-lock.json, package.json, app.module.ts and data.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,21 +8,28 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Data } from './data/data.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			name: 'Data',
-			type: 'mysql',
-			host: 'localhost',
-			port: 3306,
-			username: 'admin',
-			password: 'Vanh@Mysql2006',
-			database: 'typegraphql',
-			synchronize: true,
-			logging: true,
-			autoLoadEntities: true,
-			entities: [Data],
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'mysql',
+				host: configService.get('DB_HOST'),
+				port: configService.get('DB_PORT'),
+				username: configService.get('DB_USERNAME'),
+				password: configService.get('DB_PASSWORD'),
+				database: configService.get('DB_NAME'),
+				synchronize: true,
+				logging: true,
+				autoLoadEntities: true,
+				entities: [Data],
+			}),
+			inject: [ConfigService],
 		}),
 		TypeOrmModule.forFeature([Data]),
 		UserModule,
