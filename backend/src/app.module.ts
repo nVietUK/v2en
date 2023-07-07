dc2538d697bb867b4c7139aee27c96b56060a86c
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-07-07 18:30:58 +0700

    update schema.gql and app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,28 @@ import { UserModule } from './user/user.module';
 import { DataModule } from './data/data.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataRepository } from './data/data.repository';
 import { Data } from './data/data.entity';
+import { IsDataExistedByHashValueConstraint } from './data/data.validator';
+
+export const myConnectionOptions = async (
+	configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => {
+	return {
+		type: 'mysql',
+		host: configService.get('DB_HOST'),
+		port: configService.get('DB_PORT'),
+		username: configService.get('DB_USERNAME'),
+		password: configService.get('DB_PASSWORD'),
+		database: configService.get('DB_NAME'),
+		synchronize: true,
+		logging: true,
+		autoLoadEntities: true,
+		entities: [Data],
+	};
+};
 
 @Module({
 	imports: [
@@ -17,21 +35,10 @@ import { Data } from './data/data.entity';
 		}),
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
-			useFactory: async (configService: ConfigService) => ({
-				type: 'mysql',
-				host: configService.get('DB_HOST'),
-				port: configService.get('DB_PORT'),
-				username: configService.get('DB_USERNAME'),
-				password: configService.get('DB_PASSWORD'),
-				database: configService.get('DB_NAME'),
-				synchronize: true,
-				logging: true,
-				autoLoadEntities: true,
-				entities: [Data],
-			}),
+			useFactory: myConnectionOptions,
 			inject: [ConfigService],
 		}),
-		TypeOrmModule.forFeature([DataRepository]),
+		TypeOrmModule.forFeature([DataRepository, IsDataExistedByHashValueConstraint]),
 		UserModule,
 		DataModule,
 		GraphQLModule.forRoot<ApolloDriverConfig>({
