b993da64963eb5c32e7d4b1f24ef9203fcb75f0b
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-07-10 13:41:14 +0700

    update 26 files and delete 3 files

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -21,17 +21,15 @@ export default route(function (/* { store, ssrContext } */) {
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
 
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
+  return createRouter({
+      scrollBehavior: () => ({ left: 0, top: 0 }),
+      routes,
 
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(
-      process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
-    ),
-  });
-
-  return Router;
+      // Leave this as is and make changes in quasar.conf.js instead!
+      // quasar.conf.js -> build -> vueRouterMode
+      // quasar.conf.js -> build -> publicPath
+      history: createHistory(
+        process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
+      ),
+    });
 });
